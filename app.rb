af76c1891d54da57e597d31453a6d6ff7e815330
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-02-19 14:18:21 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,39 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+
+  get '/square/:number' do
+    @square = params[:number].to_i * params[:number].to_i
+    "#{@square.to_s}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number_phrase = params[:phrase] * params[:number].to_i
+    "#{@number_phrase}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @phrase = "#{params[:word1]} " + "#{params[:word2]} " + "#{params[:word3]} " +
+    "#{params[:word4]} " + "#{params[:word5]}" + "."
+    "#{@phrase}"
+  end
+
+  get '/:operation/:number1/:number2' do
+    case params[:operation]
+    when "add"
+      "#{params[:number1].to_i + params[:number2].to_i}"
+    when "subtract"
+      "#{params[:number1].to_i - params[:number2].to_i}"
+    when "multiply"
+      "#{params[:number1].to_i * params[:number2].to_i}"
+    when "divide"
+      "#{params[:number1].to_i / params[:number2].to_i}"
+    end
+  end
 
 end
